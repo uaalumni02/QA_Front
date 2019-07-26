2c153bca846a2bd98e2acdfef21e36bed9fcbe9a
Demeco <Demeco_Bell@demecos-mbp.attlocal.net>
2019-07-25 21:23:47 -0400

    add login

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ import path from 'path';
 import express from 'express';
 import ejs from 'ejs';
 import axios from 'axios'
+import bodyParser from 'body-parser';
 
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(__dirname + '/views'))
+
+app.use('/public', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+    return res.render('login')
+});
 
 app.listen(port, () => console.log('frontend is live'));
