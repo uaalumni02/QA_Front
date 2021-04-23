bd5048cf2b31e0859e1a83076fd1abeca50b7625
Demeco <Demeco_Bell@Demecos-MBP.attlocal.net>
2021-04-23 19:06:24 -0400

    update register

--- src/app.js
+++ src/app.js
@@ -1,43 +1,40 @@
-import path from 'path';
-import express from 'express';
-import ejs from 'ejs';
-import axios from 'axios'
-import bodyParser from 'body-parser';
-
+import path from "path";
+import express from "express";
+import ejs from "ejs";
+import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(__dirname + '/views'))
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(__dirname + "/views"));
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    return res.render('login')
+app.get("/", (req, res) => {
+  return res.render("login");
 });
 
-app.get('/home/:id', (req, res) => {
-    return res.render('home')
+app.get("/home/:id", (req, res) => {
+  return res.render("home");
 });
 
-app.get('/question/:id', (req, res) => {
-    return res.render('question')
+app.get("/question/:id", (req, res) => {
+  return res.render("question");
 });
 
-app.get('/topic/:id', (req, res) => {
-    return res.render('topic')
+app.get("/topic/:id", (req, res) => {
+  return res.render("topic");
 });
 
-app.get('/register', (req, res) => {
-    return res.render('register')
+app.get("/register", (req, res) => {
+  return res.render("register");
 });
 
-
-
-app.listen(port, () => console.log('frontend is live'));
+app.listen(port, () => console.log("frontend is live"));
